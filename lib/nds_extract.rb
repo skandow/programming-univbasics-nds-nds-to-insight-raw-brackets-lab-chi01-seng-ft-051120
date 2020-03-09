0a962a4355e06fcf7691b86ba2ed28a92e6ff570
Steven Kandow <kando1sr@gmail.com>
2020-03-09 14:10:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,7 +16,7 @@ def directors_totals(nds)
       total_gross = total_gross + nds[row_index][:movies][column_index][:worldwide_gross]
       column_index += 1
     end 
-    result << {nds[row_index][:name] => total_gross}
+    result << nds[row_index][:name] => total_gross
     row_index += 1 
   end
   result
